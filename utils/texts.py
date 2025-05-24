07c380b657ecef16b70f36c75dc2b61b3c33c681
shinubo28always <shinubo28always@gmail.com>
2025-05-24 22:10:10 +0600

    Create texts.py

---
+++ utils/texts.py
@@ -0,0 +1,20 @@
+START_TEXT = """\
+**Hey {mention},**
+I'm an image to sticker bot!
+
+Send me any image and I'll convert it into a sticker **without resizing**.
+"""
+
+HELP_TEXT = """**Help Menu**
+
+• Just send me an image.
+• I’ll convert it into a sticker without compressing or resizing it.
+"""
+
+ABOUT_TEXT = """**About This Bot**
+
+• Developer: @AniReal_Support
+• Language: Python
+• Library: Pyrogram
+• Purpose: Convert images to full-size stickers
+"""
